0131197ff4067bbf443251563e8b083a93f4861f
Mitesh Dandade <miteshdandade@ymail.com>
2022-01-28 20:17:22 +0530

    Update performance-test.js

--- performance-test.js
+++ performance-test.js
@@ -4,8 +4,10 @@ import http from 'k6/http';
 export const options = {
   duration: '1m',
   vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+  },
 };
-
 
 export default function () {
   http.get('http://test.k6.io/contacts.php');
