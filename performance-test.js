fdcb36d8256026fcdde9da94d42db50134601292
Mitesh Dandade <miteshdandade@ymail.com>
2022-01-31 19:29:49 +0530

    Update performance-test.js

--- performance-test.js
+++ performance-test.js
@@ -3,7 +3,7 @@
 import { sleep, group } from 'k6'
 import http from 'k6/http'
 
-export const options = { vus: 10, duration: '5m' }
+export const options = { vus: 5, duration: '1m' }
 
 export default function main() {
   let response
