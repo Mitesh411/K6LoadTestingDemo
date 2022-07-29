e4a697bb31d0deb2a468dc9413d0b5f27b0622d6
Mitesh Dandade <miteshdandade@ymail.com>
2022-07-29 12:26:01 +0530

    Update performance-test.js

--- performance-test.js
+++ performance-test.js
@@ -5,7 +5,7 @@ import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 
 
-export const options = { vus: 1, 
+export const options = { vus: 300, 
                         duration: '1m' ,
                          insecureSkipTLSVerify: true
                        }
