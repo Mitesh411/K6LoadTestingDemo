e560a2a865f7cd35347130cb09f8206fdafa078f
Mitesh Dandade <miteshdandade@ymail.com>
2022-07-29 12:00:48 +0530

    Update performance-test.js

--- performance-test.js
+++ performance-test.js
@@ -5,27 +5,99 @@ import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 
 
-export const options = { vus: 5, duration: '1m' }
+export const options = { vus: 300, duration: '5m' }
 
 export default function main() {
   let response
 
-  group('page_1 - https://letcode.in/', function () {
-    response = http.get('https://test.k6.io', {
+  group('page_1 - https://isityou.gg/', function () {
+    response = http.get('https://isityou.gg/', {
       headers: {
         'upgrade-insecure-requests': '1',
-        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+      },
+    })
+    sleep(45.7)
+  })
+
+  group('page_2 - https://isityou.gg/game-info', function () {
+    response = http.get('https://isityou.gg/game-info', {
+      headers: {
+        'upgrade-insecure-requests': '1',
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+      },
+    })
+    sleep(7.7)
+  })
+
+  group('page_3 - https://isityou.gg/faq', function () {
+    response = http.get('https://isityou.gg/faq', {
+      headers: {
+        'upgrade-insecure-requests': '1',
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+      },
+    })
+    sleep(4.2)
+  })
+
+  group('page_4 - https://isityou.gg/contact', function () {
+    response = http.get('https://isityou.gg/contact', {
+      headers: {
+        'upgrade-insecure-requests': '1',
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+      },
+    })
+    sleep(0.8)
+
+    response = http.get('https://isityou.gg/captchaApi', {
+      headers: {
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+      },
+    })
+    sleep(11.3)
+
+    response = http.post(
+      'https://isityou.gg/contact',
+      {
+        firstName: '',
+        lastName: '',
+        emailId: '',
+        category: '',
+        captcha: '',
+        commentInput: '',
+      },
+      {
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          origin: 'https://isityou.gg',
+          'upgrade-insecure-requests': '1',
+          'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+        },
+      }
+    )
+    sleep(0.8)
+
+    response = http.get('https://isityou.gg/captchaApi', {
+      headers: {
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
       },
     })
   })
-
- 
-  // Automatically added sleep
-  sleep(1)
 }
-
 export function handleSummary(data) {
   return {
     "result.html": htmlReport(data),
